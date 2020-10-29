019a95b5f345650e32ea6f82eb14834c508bfd73
Valery Gurov <va1ery@ya.ru>
2020-10-29 16:00:25 +0300

    Entity Class added

--- InventoryDatabaseCore/InventoryDbContext.cs
+++ InventoryDatabaseCore/InventoryDbContext.cs
@@ -1,13 +1,34 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using InventoryModels;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using System;
+using System.IO;
 
 namespace InventoryDatabaseCore
 {
     public class InventoryDbContext : DbContext
     {
+        private static IConfigurationRoot _configuration;
+        public DbSet<Item> Items { get; set; }
+        public InventoryDbContext() : base() { }
         public InventoryDbContext(DbContextOptions options)
         : base()
         {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder
+optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true,
+                reloadOnChange: true);
+                _configuration = builder.Build();
+                var cnstr = _configuration.GetConnectionString("InventoryManager");
+                optionsBuilder.UseSqlServer(cnstr);
+            }
         }
     }
 }
